9e6653285aa44bd3888370d576ed1fd1d56d4953
tracyspacy <42025315+tracyspacy@users.noreply.github.com>
2026-05-15 11:46:10 +0200

    switched from reqwest to ureq

--- src/api.rs
+++ src/api.rs
@@ -2,26 +2,26 @@ use crate::config::ApiConfig;
 use crate::post::{ApiResponse, Post};
 
 pub struct ApiClient {
-    pub client: reqwest::blocking::Client,
+    pub client: ureq::Agent,
     pub api_config: ApiConfig,
 }
 
 impl ApiClient {
     pub fn new(api_config: ApiConfig) -> Self {
         Self {
-            client: reqwest::blocking::Client::builder()
-                .user_agent("lemmy-tray")
-                .build()
-                .unwrap(),
+            client: ureq::Agent::new_with_defaults(),
             api_config,
         }
     }
+    //Todo:add error handling
     pub fn get_post(&self) -> Post {
         let mut response: ApiResponse = self
             .client
             .get(self.api_config.build_url())
-            .send()
-            .and_then(|r| r.json())
+            .call()
+            .unwrap()
+            .body_mut()
+            .read_json()
             .unwrap();
 
         (&response.posts.remove(0)).into()
